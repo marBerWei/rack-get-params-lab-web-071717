62b4e9842dbdbd5d597f48154fd62cab00d06f56
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-07 13:54:59 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,46 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    @resp = Rack::Response.new
+    @req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if @req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
+        @resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+    elsif @req.path.match(/search/)
+      search_term = @req.params["q"]
+      @resp.write handle_search(search_term)
+
+    elsif @req.path.match(/cart/)
+      if @@cart.length == 0
+        @resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          @resp.write "#{item}\n"
+        end
+      end
+    elsif @req.path.match(/add/)
+      search_term = @req.params["item"]
+      @resp.write add(search_term)
+
     else
-      resp.write "Path Not Found"
+      @resp.write "Path Not Found"
     end
 
-    resp.finish
+    @resp.finish
+  end
+
+  def add(item)
+    if @@items.include?(item)
+      @@cart << item 
+      @resp.write "added #{item}"
+    else
+      @resp.write "We don't have that item"
+    end
   end
 
   def handle_search(search_term)
@@ -27,4 +50,7 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+
+
 end
